c8ae04074d14bad24e8b3e972e6c839194b9fb5f
VIVEK KUMAR <91110678+iamvivekkr@users.noreply.github.com>
2022-08-21 16:34:41 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>VSHOP</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
